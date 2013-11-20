24ff30997539b60cf2240a1b8044f79ed2b9346b
xin gao <Gx@xintekiiMac.local>
2013-11-20 15:29:34 +0800

    Initial Commit

---
+++ GXOnlineDemo/gxAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  gxAppDelegate.h
+//  GXOnlineDemo
+//
+//  Created by xin gao on 13-11-20.
+//  Copyright (c) 2013年 xin gao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface gxAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GXOnlineDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GXOnlineDemo
+//
+//  Created by xin gao on 13-11-20.
+//  Copyright (c) 2013年 xin gao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "gxAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([gxAppDelegate class]));
+    }
+}
